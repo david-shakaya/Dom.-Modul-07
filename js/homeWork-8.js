4f5991b9c04d8826882b362c137fbc491b47274f
david-shakaya <devidoz666@gmail.com>
2020-11-12 12:06:41 +0200

    ль

--- js/homeWork-8.js
+++ js/homeWork-8.js
@@ -1,6 +1,30 @@
 const divControlsRef = document.querySelector('#controls')
 
-const inputControlsRef = divControlsRef.firstElementChild.textContent
-const btnRenderRef = ('divControlsRef[data-action="render"]')
+const inputControlsRef = divControlsRef.firstElementChild
+const btnRenderRef = divControlsRef.querySelector('[data-action="render"]')
+const btnDestroyRef = divControlsRef.querySelector('[data-action="destroy"]')
 
-console.log(btnRenderRef);
+console.log(btnDestroyRef);
+
+let arr = [];
+
+inputControlsRef.addEventListener('click', (event) => {
+
+    // console.log(arr.push(+event.target.value.));
+    let value = [+event.target.value] 
+    value = value.pop()
+
+    console.log(value)
+for (let i = 0; i < value; i++) {
+    // const element = array[i];
+arr.push([i])
+}
+//  console.log(value);
+
+console.log(arr);
+} )
+
+// for (let i = 0; i < arr; i++) {
+//     // const element = array[i];
+//     console.log(i);
+// }
