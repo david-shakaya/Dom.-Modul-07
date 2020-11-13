a2339cd451fe17c8d1818ab5aa42b2cbd3349c0b
david-shakaya <devidoz666@gmail.com>
2020-11-13 19:09:15 +0200

    +++

--- js/homeWork-8.js
+++ js/homeWork-8.js
@@ -9,7 +9,6 @@ const btnDestroyRef = divControlsRef.querySelector('[data-action="destroy"]')
 const divRef = document.querySelector('div#boxes')
 
 
-
 let divFindRef;
 
 
@@ -27,33 +26,52 @@ btnDestroyRef.addEventListener('click', () =>  createDiv(destroyBox()))
  }
 //   уничтожает боксы
 function destroyBox() {
-    return (inputControlsRef.value = 0,
+    inputControlsRef.value = 0
+    // box.style.width = size = 30
+    // box.style.height  = size = 30
     
-    divFindRef.remove())
-    
-    
+     while (divRef.firstChild) { 
+         divRef.firstChild.remove()
+    }
     
  }
 
 //   создает боксы
 function createDiv(amaunt) {
-    for (let i = 0; i < amaunt; i +=1) {
+    for (let i = 0; i < amaunt; i += 1) {
         
         box = document.createElement('div')
         box.classList.add('box')
+        box.style.backgroundColor = elementRand();
+        box.style.width = abbSize() ;
         divRef.append(box)
+        console.log(divRef);
     }
-    divFindRef = document.querySelectorAll('.box')
-    console.log(divFindRef);
+    divFindRef = document.querySelector('.box')
+    // let i = 3;
+   
 }
 
 
 
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
+function elementRand() {
+    box.style.backgroundColor =
+        'rgb(' + getRandomInt(0, 255) + ', '
+        + getRandomInt(0, 255) +
+        ', ' + getRandomInt(0, 255) + ')';
+  };
 
-
-// btnRenderRef.addEventListener('click', () =>{
-
+let width = 30
+  let height = 30
+function abbSize() {
     
-// })
+    box.style.width = width += 10
+    box.style.height  = height += 10
+}
+
+
